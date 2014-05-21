780896a3cfcde67ddb892838d9a5e6330d88807d
James Jurkiewicz <james.jurkiewicz@hertford.ox.ac.uk>
2014-05-21 10:29:14 +0100

    back with pony

--- production_pony_options.rb
+++ production_pony_options.rb
@@ -3,10 +3,11 @@ Pony.options = {
   :via_options => {
     :address => 'smtp.sendgrid.net',
     :port => '587',
-    :domain => 'heroku.com',
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'bridgeofsighsball.co.uk',
+    :user_name => 'tickets@bridgeofsighsball.co.uk',
+    :password => 'bosb4',
     :authentication => :plain,
     :enable_starttls_auto => true
   }
 }
+
