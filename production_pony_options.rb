1b8e2fafa4e68847ddbde4a03258f0b4ae383481
James Jurkiewicz <james.jurkiewicz@hertford.ox.ac.uk>
2014-05-21 10:36:38 +0100

    back with pony

--- production_pony_options.rb
+++ production_pony_options.rb
@@ -1,13 +1,39 @@
+# Pony.options = {
+#   :via => :smtp,
+#   :via_options => {
+#     :address => 'smtp.sendgrid.net',
+#     :port => '587',
+#     :domain => 'heroku.com',
+#     :user_name => ENV['SENDGRID_USERNAME'],
+#     :password => ENV['SENDGRID_PASSWORD'],
+#     :authentication => :plain,
+#     :enable_starttls_auto => true
+#   }
+# }
+
 Pony.options = {
   :via => :smtp,
   :via_options => {
-    :address => 'smtp.sendgrid.net',
+    :address => 'smtp.iomartmail.net',
     :port => '587',
     :domain => 'bridgeofsighsball.co.uk',
-    :user_name => 'tickets@bridgeofsighsball.co.uk',
-    :password => 'bosb4',
+    :user_name => 'tickets@bridgeofsighsball',
+    :password => 'bsob4',
     :authentication => :plain,
     :enable_starttls_auto => true
   }
 }
 
+# Pony.options = {
+#   :via => 'smtp',
+#   :from => 'Bridge of Sighs Ball ticket sales <tickets@bridgeofsighsball.co.uk>',
+#   :via_options => {
+#     :address              => 'smtp.iomartmail.com',
+#     :port                 => '587',
+#     :enable_starttls_auto => true,
+#     :user_name            => 'tickets@bridgeofsighsball.co.uk',
+#     :password             => 'bosb4',
+#     :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+#     :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+#   }
+# }
