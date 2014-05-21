310193b91acaaa02db6551e8949b66d09aec0f6a
James Jurkiewicz <james.jurkiewicz@hertford.ox.ac.uk>
2014-05-21 10:43:08 +0100

    back with pony

--- production_pony_options.rb
+++ production_pony_options.rb
@@ -11,29 +11,29 @@
 #   }
 # }
 
-Pony.options = {
-  :via => :smtp,
-  :via_options => {
-    :address => 'smtp.iomartmail.net',
-    :port => '587',
-    :domain => 'bridgeofsighsball.co.uk',
-    :user_name => 'tickets@bridgeofsighsball',
-    :password => 'bsob4',
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
-}
-
 # Pony.options = {
-#   :via => 'smtp',
-#   :from => 'Bridge of Sighs Ball ticket sales <tickets@bridgeofsighsball.co.uk>',
+#   :via => :smtp,
 #   :via_options => {
-#     :address              => 'smtp.iomartmail.com',
-#     :port                 => '587',
-#     :enable_starttls_auto => true,
-#     :user_name            => 'tickets@bridgeofsighsball.co.uk',
-#     :password             => 'bosb4',
-#     :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-#     :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+#     :address => 'smtp.iomartmail.net',
+#     :port => '587',
+#     :domain => 'bridgeofsighsball.co.uk',
+#     :user_name => 'tickets@bridgeofsighsball',
+#     :password => 'bsob4',
+#     :authentication => :plain,
+#     :enable_starttls_auto => true
 #   }
 # }
+
+Pony.options = {
+  :via => 'smtp',
+  :from => 'Bridge of Sighs Ball ticket sales <tickets@bridgeofsighsball.co.uk>',
+  :via_options => {
+    :address              => 'smtp.iomartmail.com',
+    :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => 'tickets@bridgeofsighsball.co.uk',
+    :password             => 'bosb4',
+    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+  }
+}
