e56e9f3b9500df7266c7b0fe4c951885610b8e2a
James Jurkiewicz <james.jurkiewicz@hertford.ox.ac.uk>
2015-01-28 21:45:11 +0000

    ruby worldpay

--- app.rb
+++ app.rb
@@ -90,24 +90,11 @@ post '/sign_up' do
   amount= params[:amount]
   group = params[:group] 
     
-def self.uri
-     if Worldpay.test?
--      "https://select-test.wp3.rbsworldpay.com/wcc/purchase"
-+      "https://select-test.worldpay.com/wcc/purchase"
-     else
--      "https://secure.wp3.rbsworldpay.com/wcc/purchase"
-+      "https://secure.worldpay.com/wcc/purchase"
-     end
-   end
-   
+
   @dancer=Booked_clients.new(:name => name, :email => email, :phone => phone,  :disclaimer => disclaimer, :terms => terms, :amount => amount, :group => group)
   @dancer.save
 
-=begin
 
-    @diner=Booked_clients.new(:name => name)
-    @diner.save
-=end
 
 
  # if params[:disclaimer]= "confirmed"
@@ -135,17 +122,17 @@ def self.uri
   
     @dancer=Booked_clients.new(:name => name, :email => email, :class => classes, :phone => phone, :disclaimer => disclaimer, :terms => terms, :amount => amount)
     @dancer.save
-
+=end
     # email words:
-    if @classes=="jan lvl 1 6:45pm"
+    if group=="jan lvl 1 6:45pm"
       @day = "Thursday 5th March"
-    elsif @classes == "jan lvl 1 7:45pm"
+    elsif group == "jan lvl 1 7:45pm"
       @day = "Thursday 5th March"
     else @day = "Friday 5th March"
     end
 
       puts @day
-      @time= @classes.split.last
+      @time= @group.split.last
       puts @time
     if settings.environment == :production
       # if we're on heroku, use the sendgrid settings
@@ -184,7 +171,84 @@ def self.uri
  else
     erb :failure 
 =end
- 
+end
 
-  
+post '/party' do
+  if params[:disclaimer]= "confirmed"
+    @groups = params[:group] 
+    @name=   params[:name].split.first.capitalize
+    @email=  params[:email]
+    @date=   params[:date]
+
+    if params[:party]="5 people"
+      @cost=125
+    else
+      @cost=200
+    end
+
+
+    name=params[:name]
+    email=params[:email]
+    phone=params[:phone] 
+    disclaimer= params[:disclaimer]
+    terms= params[:terms]
+    amount= params[:amount]
+    group = params[:group] 
+      
+
+    @party=Party.new(:name => name, :email => email, :phone => phone,  :disclaimer => disclaimer, :terms => terms, :amount => amount, :group => group)
+    @party.save
+
+
+    Pony.mail(
+      :to => @email,
+      :subject => "Body and Pole Gsy party confirmation",
+      :body => erb(:email_party, :layout => false),
+    # :bcc => anneka@...
+      :attachments => {"H&F_Declaration.docx" => File.read("public/H&F_Declaration.docx")},
+      :via => 'smtp',
+      :from => 'Body & Pole Limited',
+      :via => :smtp,
+      :via_options => {
+        :address              => 'smtp.gmail.com',
+        :port                 => '587',
+        :enable_starttls_auto => true,
+        :user_name            => 'bodyandpole.gsy@gmail.com',
+        :password             => '9carryonbrynn99',
+        :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+        :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+    })
+    erb :thankyou_party
+
+  else 
+    erb :no_disclaimer
+  end
+end
+
+post '/contact' do
+  @classes = params[:class] 
+  @name=   params[:name]
+  @email=  params[:email]
+  @subject=  params[:subject]
+  @message=  params[:message]
+
+  Pony.mail(
+    :to => @email,
+    :subject => "Body & Pole Gsy contact received",
+    :body => erb(:contact_email, :layout => false),
+    :bcc => 'bodyandpole.gsy@gmail.com',
+    :via => 'smtp',
+    :from => 'Body & Pole Limited',
+    :via => :smtp,
+    :via_options => {
+      :address              => 'smtp.gmail.com',
+      :port                 => '587',
+      :enable_starttls_auto => true,
+      :user_name            => 'bodyandpole.gsy@gmail.com',
+      :password             => '9carryonbrynn99',
+      :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+      :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+  })
+
+  erb :contact_thankyou
 end
