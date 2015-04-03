7c518afa64aafbecfa2ef90276948df52a05203d
James Jurkiewicz <james.jurkiewicz@hertford.ox.ac.uk>
2015-04-03 07:47:01 +0100

    cap at 10

--- app.rb
+++ app.rb
@@ -95,7 +95,10 @@ post '/sign_up' do
   @group = params[:group]
   
   if Booked_clients.where( :group => @group ).count <= 2
+    @x= Booked_clients.where( :group => @group ).count
     erb :failure
+    puts "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    puts @x
   else
 
     @dancer=Booked_clients.new(:name => name, :email => email, :phone => phone,  :disclaimer => disclaimer, :terms => terms, :amount => @amount, :group => group)
